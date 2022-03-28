d714b3c6a783c5948aeb4cab5838bd87f01acd49
Kyle Stetz <kstetz3@live.com>
2022-03-28 16:55:51 -0400

    get text from an input box and add it to an array

--- controller.js
+++ controller.js
@@ -1,13 +1,21 @@
 let itemCounter = 1;
 let itemArray = [];
+
+
 document.querySelector("#btnAlert").addEventListener("click",useBtn);
+document.querySelector("#btnList").addEventListener("click", inputList);
+
 function useBtn(event){
-   
+    
     itemArray.push(itemCounter);
     itemCounter = itemCounter+1;
-    printList()
-    return itemArray;
+    printList();
 }
    function printList(){
         alert("Here is a list of items " + itemArray);
+}
+function inputList(event){
+    let listItem = document.querySelector("#ListItem").value;
+    itemArray.push(listItem);
+    printList();
 }
